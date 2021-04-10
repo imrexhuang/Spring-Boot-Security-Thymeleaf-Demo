521cc3befb37c7daf94b5c029c0bf0d79fd65b4e
Rex <im@rex.tw>
2021-04-10 08:39:20 +0800

    預留關閉關閉熱部署(Hot Swap)的程式碼

--- src/main/java/org/inlighting/security/SecurityDemoApplication.java
+++ src/main/java/org/inlighting/security/SecurityDemoApplication.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 public class SecurityDemoApplication {
 
     public static void main(String[] args) {
+        // https://docs.spring.io/spring-boot/docs/1.5.16.RELEASE/reference/html/using-boot-devtools.html
+        //System.setProperty("spring.devtools.restart.enabled", "false"); //false表示關閉熱部署(Hot Swap)
+
         SpringApplication.run(SecurityDemoApplication.class, args);
     }
 }
